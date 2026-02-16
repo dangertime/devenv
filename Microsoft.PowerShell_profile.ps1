e49958aa3cccb46477e86158c34bb118b52b75a6
dangertime <15330685+dangertime@users.noreply.github.com>
2026-02-16 00:00:20 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,4 @@
-$env:POSH_GIT_ENABLED = $true
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme ~/.omp/mytheme.omp.json
-
-# posh-git settings
-$GitPromptSettings.BranchColor.ForegroundColor = 0xC591E8
-
-$GitPromptSettings.BeforeStatus.Text = ""
-$GitPromptSettings.AfterStatus.Text = ""
-#$GitPromptSettings.BeforeStatus.ForegroundColor = 0x5FAAE8
-# $GitPromptSettings.AfterStatus.ForegroundColor = 0x5FAAE8
+oh-my-posh init pwsh --config ~/OneDrive/Documents/PowerShell/mytheme.omp.json | Invoke-Expression
 
 function gs() { git status }
 function gaa() { git add --all }
@@ -17,4 +6,23 @@ function gc() { git commit }
 function gp() { git push }
 function gb() { git branch }
 function gll() { git log }
-function cdg() {cd C:\Git\}
+function cdg() {cd C:\repos\}
+
+# Linux/bash aliases
+function ll() { Get-ChildItem -Force @args }
+function la() { Get-ChildItem -Force @args }
+function ..() { Set-Location .. }
+function ...() { Set-Location ../.. }
+function touch($file) {
+    if (Test-Path $file) { (Get-Item $file).LastWriteTime = Get-Date }
+    else { New-Item $file -ItemType File }
+}
+function mkcd($dir) { New-Item -ItemType Directory -Path $dir | Set-Location }
+function which($cmd) { Get-Command $cmd | Select-Object -ExpandProperty Source }
+function head { param($n = 10) $input | Select-Object -First $n }
+function tail { param($n = 10) $input | Select-Object -Last $n }
+function df() { Get-PSDrive -PSProvider FileSystem | Format-Table Name, @{N='Used(GB)';E={[math]::Round($_.Used/1GB,2)}}, @{N='Free(GB)';E={[math]::Round($_.Free/1GB,2)}} }
+function export($var) {
+    $name, $value = $var -split '=', 2
+    Set-Item -Path "env:$name" -Value $value
+}
